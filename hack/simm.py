09d3084645e456a3d172c96f7c1a194fec44f861
nikithasrikanth1711@gmail.com <nikithasrikanth1711@gmail.com>
2019-08-25 16:47:39 +0530

    Final push

--- hack/simm.py
+++ hack/simm.py
@@ -1,28 +1,55 @@
 import cv2
 import numpy as np
-#filepath1=input()
-#filepath2=input()
-img1 = cv2.imread("pikachu_copy.png")
-img2 = cv2.imread("pcopy.png")
+def dist(xa,ya,za,xb,yb,zb):
+    dist = np.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb)+(za-zb)*(za-zb))
+    return dist
+#f1 is the first image
+#f2 is the second image
+img1 = cv2.imread("f1.png")
+img20 = cv2.imread("f2.png")
+h1,w1=img1.shape[:2]
+#w2,h2=img20.shape[:2]
+#if(w1>w2):
+#    w=w1
+#else:
+#    w=w2
+#if(h1>h2):
+#    h=h1
+#else:
+#    h=h2
 
-w,h=img1.shape[:2]
-img = np.ones((w,h,3))
+
+img2=cv2.resize(img20,(w1,h1))
+#print(img2.shape,img1.shape)
+img = np.zeros((h1,w1,3))
 count=0
-for i in range(h):
-    for j in range(w):
+for i in range(h1):
+    for j in range(w1):
         color1=img1[i,j]
         color2=img2[i,j]
-        
-        p=[0,0,0]
-        for k in range(3):
-            if(abs(color1[k]-color2[k])<45):
-                p[k]=1
-        if(sum(p)>=2):
+
+#         p=[0,0,0]
+#        for k in range(3):
+#            if(abs(color1[k]-color2[k])<20):
+#                p[k]=1
+#        if(sum(p)>=2):
+        d=dist(color1[0],color1[1],color1[2],color2[0],color2[1],color2[2])
+        if(d<0.2):
             count+=1
-            img[i,j]=[0,0,0]
-percentage=count/(h*w)*100
+            img[i,j]=[0,255,0]
+        elif(d<0.4):
+            count+=1
+            img[i,j]=[255,0,0]
+        elif(d<0.6):
+            count+=1
+            img[i,j]=[0,0,255]
+
+
+
+percentage=count/(h1*w1)*100
+print(percentage)
 cv2.imshow("outputimg",img)
 cv2.waitKey(0)
-print(percentage)
+
 
 
